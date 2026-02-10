6cb22bb9098d5fef7dcdc657830cf48a4355bfd1
Mathis Burger <handymathis@gmail.com>
2026-02-10 19:47:10 +0000

    feat: Added protobuf stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use prost::Message;
+
+use crate::esp::EspStatusMessage;
+
+pub mod esp {
+    include!(concat!(env!("OUT_DIR"), "/esp_status.rs"));
+}
+
 fn main() {
     println!("Hello, world!");
 }
